c91e88ced254a16938b167b9dcd4cdad889289d1
AgesilausDrako <agesilaus@yahoo.com>
2017-09-12 11:39:50 -0400

    added models and config from sequelize

--- models/burger.js
+++ models/burger.js
@@ -1,45 +1,16 @@
-// Import the ORM to create functions that will interact with the database.
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
-
-// Creates a "Character" model that matches up with DB
-var Burger = sequelize.define("burger", {
-  // the routeName gets saved as a string
-  routeName: Sequelize.STRING,
-  // the name of the character (a string)
-  burger_name: Sequelize.STRING,
-  // the character's role (a string)
-  devoured: Sequelize.BOOLEAN,
-}, {
-  timestamps: false
-});
-
-// Syncs with DB
-Burger.sync();
-
-// // Makes the Character Model available for other files (will also create a table)
-// module.exports = Burger;
-// var orm = require("../config/orm.js");
-
-// var burger = {
-//   selectAll: function(cb) {
-//     orm.selectAll("burgers", function(res) {
-//       cb(res);
-//     });
-//   },
-//   // The variables cols and vals are arrays.
-//   insertOne: function(cols, vals, cb) {
-//     orm.insertOne("burgers", cols, vals, function(res) {
-//       cb(res);
-//     });
-//   },
-//   updateOne: function(objColVals, condition, cb) {
-//     orm.updateOne("burgers", objColVals, condition, function(res) {
-//       cb(res);
-//     });
-//   }
-// };
-
-// // Export the database functions for the controller (burgers_controller.js).
-// module.exports = burger;
+module.exports = function(sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+      text: {
+        burger_name: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 140]
+        }
+      },
+        devoured: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      }
+    });
+    return Burger;
+  };
